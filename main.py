7dfb5186907826f8c6d45532eba56ebf72f4c91e
Shasss <shaswatkumar447@gmail.com>
2024-10-28 20:44:00 +0530

    backprop added training testing done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from data import train_data,test_data
 import numpy as np
 from rnn import RNN
+import random
+
 
 vocab=list(set([w for i in train_data.keys() for w in i.split(' ')]))
 vc_sz=len(vocab)
@@ -21,7 +23,35 @@ def softmax(xs):
         return np.exp(xs)/sum(np.exp(xs))
 
 rnn=RNN(vc_sz,2)
-i=createInputs('i am very good')
-o,h=rnn.forward(i)
-pr=softmax(o)
-print(pr)
+def processData(data,backprop):
+     itm=list(data.items())
+     random.shuffle(itm)
+
+     loss=0
+     num_c=0
+     for x,y in itm:
+       inp=createInputs(x)
+       tg=int(y)
+
+       out,_=rnn.forward(inp)
+       prbs=softmax(out)
+
+       loss-=np.log(prbs[tg])
+       num_c+=int(np.argmax(prbs)==tg)
+
+       if backprop:
+         dl_dy=prbs
+         dl_dy[tg]-=1
+
+         rnn.backprop(dl_dy)
+
+     return loss/len(data),num_c/len(data)
+
+for epoch in range(1000):
+    train_loss,train_acc=processData(train_data,True)
+
+    if epoch%100==99:
+        print('Epoch %d' % (epoch + 1))
+        print('Train:\tLoss %.3f | Accuracy: %.3f' % (train_loss.item(), train_acc))
+        test_loss,test_acc=processData(test_data,False)
+        print('Test:\tLoss %.3f | Accuracy: %.3f' % (test_loss.item(), test_acc))
--- rnn.py
+++ rnn.py
@@ -14,8 +14,41 @@ class RNN:
     def forward(self,inputs):
         h=np.zeros((self.Whh.shape[0],1))
 
-        for _,x in enumerate(inputs):
+        self.ls_in=inputs
+        self.ls_h={0:h}
+
+        for i,x in enumerate(inputs):
             h=np.tanh(self.Wxh@x+self.Whh@h+self.bxh)
+            self.ls_h[i+1]=h
+
         y=self.Wyh@h+self.byh
-        
+
         return y,h
+    
+    def backprop(self,dy,lr=2e-2):
+        n=len(self.ls_in)
+
+        d_why=dy@self.ls_h[n].T
+        d_byh=dy
+
+        d_whh=np.zeros(self.Whh.shape)
+        d_wxh=np.zeros(self.Wxh.shape)
+        d_bxh=np.zeros(self.bxh.shape)
+
+        dh=self.Wyh.T@dy
+        
+        for t in reversed(range(n)):
+            tp=((1-self.ls_h[t+1]**2)*dh)
+            d_bxh+=tp
+            d_whh+=tp@self.ls_h[t].T
+            d_wxh+=tp@self.ls_in[t].T
+            dh=self.Whh.T@tp
+        
+        for d in [d_wxh,d_whh,d_why,d_bxh,d_byh]:
+            np.clip(d,-1,1,out=d)
+
+        self.Whh-=lr*d_whh
+        self.Wxh-=lr*d_wxh
+        self.Wyh-=lr*d_why
+        self.bxh-=lr*d_bxh
+        self.byh-=lr*d_byh
